3ceeb4514428a4ad2875d0bd8862f5b992bf2e90
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2026-04-04 16:26:03 +0300

    Time: 100 ms (62.91%), Space: 48.6 MB (84%) - LeetHub

---
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        res = []
+        j = 0  # pointer for spaces
+        
+        for i in range(len(s)):
+            if j < len(spaces) and i == spaces[j]:
+                res.append(' ')
+                j += 1
+            res.append(s[i])
+        
+        return ''.join(res)
